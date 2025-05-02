5751c2ac5823b6e9256e14e10555798b4e509b9f
Gaspare Joubert <gasparejoubert@gmail.com>
2025-05-02 17:56:24 +0100

    Add RouteServiceProvider to manage route configurations
    Introduce a new RouteServiceProvider class to handle route model bindings, pattern filters, and grouping of API and web routes. Also define a rate limiter for API requests to enhance security and resource management. Update the providers list to include this new service provider.

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file api.php
+ *
+ * @author Gaspare Joubert
+ * @date 02/05/2025 15:58
+ *
+ */
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// API routes
+
