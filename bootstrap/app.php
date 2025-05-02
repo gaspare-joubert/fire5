ed96379d19b9f0b500d57268c3027b2521add5e6
Gaspare Joubert <gasparejoubert@gmail.com>
2025-05-02 18:41:51 +0100

    Add Laravel Sanctum for API authentication
    Integrated Laravel Sanctum to enable token-based authentication for APIs and SPAs. Changes include adding the Sanctum package, configuring middleware, creating database migrations for personal access tokens, and updating the `User` model to leverage Sanctum's `HasApiTokens` trait.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,16 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+return Application::configure(basePath: dirname(__DIR__))->withRouting(
+    web:      __DIR__ . '/../routes/web.php',
+    commands: __DIR__ . '/../routes/console.php',
+    health:   '/up',
+)->withMiddleware(function (Middleware $middleware) {
+    $middleware->api(append: [
+                                 EnsureFrontendRequestsAreStateful::class,
+                             ]);
+})->withExceptions(function (Exceptions $exceptions) {
+    //
+})->create();
